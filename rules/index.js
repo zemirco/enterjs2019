fea8441b159162215b2935fec4b7094ff931c429
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-06-11 16:41:20 +0200

    make rule work

--- rules/index.js
+++ rules/index.js
@@ -1,18 +1,26 @@
+
+const de = require('../de')
+
 module.exports = {
   rules: {
     'no-unused-keys': {
-      create() {
+      create(context) {
         return {
           CallExpression: node => {
             const { callee } = node
             if (callee.type === 'MemberExpression') {
               if (callee.object.name === 'i18n') {
                 if (callee.property.name === 'transl') {
-                  // console.log(node.arguments[0].value)
+                  const {value} = node.arguments[0]
+                  if (!de[value]) {
+                    context.report({
+                      node,
+                      message: `"${value}" does not exist in i18n files`
+                    })
+                  }
                 }
               }
             }
-            // console.log('---')
           }
         }
       }
